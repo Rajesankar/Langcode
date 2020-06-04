5078073a28dffffb7b24758b4cb532d079073559
Rajesankar <rajesankaralingam@gmail.com>
2020-06-04 12:41:26 -0500

    code added

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,27 @@
+package org.lang;
+
+public class LanguageInfo {
+	private void tamilLan() {
+		System.out.println("People speaks Tamil 50%");
+
+	}private void englan() {
+		System.out.println("People speaks English 30%");
+
+	}private void hindiLan() {
+		System.out.println("People speaks Hindi 20%");
+
+	}
+	
+
+	public static void main(String[] args) {
+		
+		LanguageInfo i=new LanguageInfo();
+		i.tamilLan();
+		i.englan();
+		i.hindiLan();
+		
+		
+		
+	}
+
+}
